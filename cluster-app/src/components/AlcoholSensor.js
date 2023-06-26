1a1ac3224061dea3fe1f386a8f43c6c4f0516182
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2023-06-26 10:20:35 +0900

    cluster-app: AlcoholSensor: Separate common style
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- cluster-app/src/components/AlcoholSensor.js
+++ cluster-app/src/components/AlcoholSensor.js
@@ -10,42 +10,39 @@ export const AlcoholSensor = (props) => {
         setMaxValue(props.max_val);
     },[props])
 
-    let _style_alert = {
+    let _style_common = {
         border: "solid 3px",
         borderRadius: "10px",
-        padding: "30px",
         margin: "2px",
+        left: "50%",
+        letterSpacing: "2px",
+        position: "absolute",
+    }
+    let _style_alert = {
+        ..._style_common,
+        padding: "30px",
         color: "#000000",
         background: "#cc0000",
         borderColor: "#ff0000",
         animation: "flash 0.4s linear infinite",
 
-        position: "absolute",
         color: "#cfe4f9",
         fontSize: 3.0*props.scale+"em",
-        letterSpacing: "2px",
         fontWeight: "900",
         top: "50%",
-        left: "50%",
         transform: "translate(-50%, -50%)",
     }
-    if (val < 0.1500) {
-        _style_alert.display = "none"
-    }
-
     let _style = {
-        border: "solid 3px",
-        borderRadius: "10px",
-        margin: "2px",
-        borderColor: "#6091d3",
-
-        position: "absolute",
-        left: "50%",
+        ..._style_common,
         transform: "translate(-50%, 0%)",
         bottom: "10px",
         color: "#cfe4f9",
+        borderColor: "#6091d3",
         fontSize: 2.2*props.scale+"em",
-        letterSpacing: "2px",
+    }
+
+    if (val < 0.1500) {
+        _style_alert.display = "none"
     }
     if (val < 0) {
         _style.display = "none"
